4047e00c415ad9baef4b4864a856a2ef74023055
Niels Dequeker <niels.dequeker@gmail.com>
2013-12-09 13:11:05 +0100

    Removed jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,17 +99,6 @@ module.exports = function (grunt) {
             },
             server: '.tmp'
         },
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                '<%= yeoman.app %>/scripts/{,*/}*.js',
-                '!<%= yeoman.app %>/scripts/vendor/*',
-                'test/spec/{,*/}*.js'
-            ]
-        },
         mocha: {
             all: {
                 options: {
@@ -299,7 +288,6 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('default', [
-        'jshint',
         'test',
         'build'
     ]);
