4bc79285724e169cc333f65e2f0720a3fcb53108
Voles <niels.dequeker@gmail.com>
2014-01-06 17:04:58 +0100

    Fixed layout issues caused by moving to Less

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,28 +99,7 @@ module.exports = function (grunt) {
             },
             server: '.tmp'
         },
-        //compass: {
-        //    options: {
-        //        sassDir: '<%= yeoman.app %>/styles',
-        //        cssDir: '.tmp/styles',
-        //        generatedImagesDir: '.tmp/images/generated',
-        //        imagesDir: '<%= yeoman.app %>/images',
-        //        javascriptsDir: '<%= yeoman.app %>/scripts',
-        //        fontsDir: '<%= yeoman.app %>/styles/fonts',
-        //        importPath: '<%= yeoman.app %>/bower_components',
-        //        httpImagesPath: '/images',
-        //        httpGeneratedImagesPath: '/images/generated',
-        //        relativeAssets: false
-        //    },
-        //    dist: {},
-        //    server: {
-        //        options: {
-        //            debugInfo: true
-        //        }
-        //    }
-        //},
-
-
+        
         less: {
           development: {
             files: {
